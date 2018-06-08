ac8bebde93a7a6d8d0aa8f899e16ced3b1040bdd
Robin-73 <cedric@mariemarthe.com>
2018-06-08 14:45:14 +0200

    Better OS Detection

--- linux_easyinstall.sh
+++ linux_easyinstall.sh
@@ -4,16 +4,41 @@
 # Date : 06.06.2018
 # This script will install a CRC Hot Wallet Masternode in the default folder location
 
-#Chekcing OS version
-version=$(lsb_release -sr)
-
-#check for version of ubuntu
-if [ $version == '16.04' ]
-then
-  echo "Running Ubuntu 16.04 : OK"
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
 else
-  echo "This script should be run on Ubuntu 16.04 only !" 
- exit 1
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+if [ "$OS" == "Ubuntu" ] && [ "$VER" == "16.04" ]; then
+  echo "$OS $VER : OK"
+else
+  echo "This script should be run on Ubuntu 16.04 only"
+  exit 1
 fi
 
 
